927a996e10a1524000f0cecf00bad4ea559aa5a4
Isaac Yauk <isaacyauk@msu.montana.edu>
2023-06-29 11:06:17 -0600

    Updates Lecture 3 notes

--- Lecture 3 - Trees pt.1/src/FileTree.java
+++ Lecture 3 - Trees pt.1/src/FileTree.java
@@ -3,19 +3,19 @@ import java.util.ArrayList;
 public class FileTree
 {
     private Node root; // tracks the root node
-    private Node current; // the node that we use to traverse the file tree; it'll tell you what "dir" you're currentl;y in
+    private Node current; // the node that we use to traverse the file tree; it'll tell you what "dir" you're currently in
 
     public FileTree()
     {
-        // Instanciates the starting roots
-        this.root = new Node("~"); // this will reperenset the parent node
+        // Instantiates the starting roots
+        this.root = new Node("~"); // this will represent the parent node
         //this.current = new Node("~"); These are NOT pointing to the same thing.
 
         // This is the way you want to set it up; have the two nodes pointing to THE SAME THING
         this.current = root;
     }
 
-    // Inserts soemthing into the tree; the bool is to denote sucess of adding someithng into the tree
+    // Inserts something into the tree; the bool is to denote success of adding something into the tree
     public boolean insert(String directory)
     {
         // Logic to check if the insertion is valid; in this case, if it's empty or a space or if it's the name of the home dir
@@ -27,7 +27,7 @@ public class FileTree
         {
             // Create the node
             Node newNode = new Node(directory);
-            newNode.setParent(current); // sets the new node's parent; current will reperesent the new node that we created
+            newNode.setParent(current); // sets the new node's parent; current will represent the new node that we created
             current.addChild(newNode);
             return true;
         }
@@ -50,5 +50,39 @@ public class FileTree
         return false; // if the loop finishes and nothing comes back; add an error message here!
     }
 
-    // TODO: continue to fill out notes from lecture 3! (MoveUp method @ 1:16:18)
+    // This could be a boolean, if you wanted
+    public void moveUp()
+    {
+        // you don't need to provide the name fo the dir because there is always exactly 1 parent, ALWAYS
+        // Check to see if you're in the edge case of moving up from the root directory
+        if(current != root)
+        {
+            current = current.getParent();
+        }
+    }
+
+    // return to the home directory
+    public void goHome()
+    {
+        current = root;
+    }
+
+    // returns the current working directory; NOT the file path
+    public String getCurrentLocation()
+    {
+        return current.getName();
+    }
+
+    // You can also return the ArrayList, for easier printing
+    public String getChildrenDirectories()
+    {
+        // create an empty string, and then iterate through the arrayList, appending each element to a string that is returned.
+        String files = "";
+        ArrayList<Node> children = current.getChildren();
+                for(Node c: children)
+                {
+                    files  += c.getName() + " ";
+                }
+                return files;
+    }
 }
