a45fa53d665d08fbf71aabae9b07a2d94001d4b1
Isaac Yauk <isaacyauk@yahoo.com>
2023-07-08 10:37:16 -0600

    Fills out Node class with getter and setter methods from class example

--- Lecture 5 - Binary Search Trees pt. 1/BST Example/src/Node.java
+++ Lecture 5 - Binary Search Trees pt. 1/BST Example/src/Node.java
@@ -1,4 +1,55 @@
 public class Node
 {
-    
+    private int value;
+    private Node left;
+    private Node right;
+    private Node parent;
+
+    public Node (int newValue)
+    {
+        this.value = newValue;
+    }
+
+    // --- Getter Methods (these return something!) ---
+    public int getValue()
+    {
+        return this.value;
+    }
+
+    public Node getLeft()
+    {
+        return this.left;
+    }
+
+    public Node getRight()
+    {
+        return this.right;
+    }
+
+    public Node getParent()
+    {
+        return this.parent;
+    }
+
+    // --- Setter Methods (these update something!) ---
+
+    public void setParent(Node newParent)
+    {
+        this.parent = newParent;
+    }
+
+    public void setLeft(Node newNode)
+    {
+        this.left = newNode;
+    }
+
+    public void setRight(Node newNode)
+    {
+        this.right = newNode;
+    }
+
+    public void setValue(int newValue)
+    {
+        this.value = newValue;
+    }
 }
