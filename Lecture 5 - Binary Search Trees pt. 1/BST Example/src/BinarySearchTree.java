b6d2a1ee54f3f1d1712d3975526b7654be5229cb
Isaac Yauk <isaacyauk@yahoo.com>
2023-07-08 10:40:53 -0600

    updates BST and BST Demo with starter code from class example

--- Lecture 5 - Binary Search Trees pt. 1/BST Example/src/BinarySearchTree.java
+++ Lecture 5 - Binary Search Trees pt. 1/BST Example/src/BinarySearchTree.java
@@ -1,4 +1,11 @@
 public class BinarySearchTree
 {
+    // need to keep track of the root node!
+    Node root;
+
+    public BinarySearchTree()
+    {
+        root = null;
+    }
 
 }
--- Lecture 5 - Binary Search Trees pt. 1/BST Example/src/BinarySearchTreeDemo.java
+++ Lecture 5 - Binary Search Trees pt. 1/BST Example/src/BinarySearchTreeDemo.java
@@ -1,4 +1,11 @@
 public class BinarySearchTreeDemo
 {
+    public static void main(String[] args)
+    {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(44);
+        bst.insert(17);
+        bst.insert(88);
+    }
 
 }
